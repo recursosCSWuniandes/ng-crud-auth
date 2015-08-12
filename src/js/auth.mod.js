d032bdc9732ffe4059334480a73a1b72097c46b3
Jhonatan Alarcón <jj.alarcon10@uniandes.edu.co>
2015-08-12 15:10:26 -0500

    Corrección Scope Se corrige scope en la directiva

--- src/js/auth.mod.js
+++ src/js/auth.mod.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,7 +6,7 @@
 
 (function (ng) {
 
-    var mod = ng.module('authModule', ['restangular', 'ngCookies']);
+    var mod = ng.module('authModule', ['restangular', 'ngCookies', 'ngRoute']);
     mod.constant('defaultStatus', {status: false});
     mod.config(['RestangularProvider', function (rp) {
             rp.setBaseUrl('webresources');
